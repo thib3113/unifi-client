04de119c1a79fe23bee13d9cb39503c38110bc03
thibaut severac <thib3113@gmail.com>
2021-07-26 23:34:59 +0200

    add websockets

--- src/test.ts
+++ src/test.ts
@@ -14,16 +14,26 @@ const main = async () => {
     await controller.login();
     await controller.initWebSockets();
 
-    //listen on all events
+    // listen on controller, for the event ctrl.connect
+    // check known events in documentation : https://thib3113.github.io/unifi-client/modules/WebSockets_events_EUnifiEvents.html
+    // else check in devtools when using the unifi web app
+    // controller websockets are only available on unifiOS
     controller.on('*', (eventName, ...args) => {
+        console.log(...args);
+    });
+
+    /**
+     * listen on super site ... seems to be an equivalent of controller websocket for non unifiOS . ( but events and name doesn't use the same form )
+     */
+    controller.superWS.on('*', (eventName, ...args) => {
         console.log(eventName, ...args);
     });
 
-    // listen on ctrl.connect
-    // check known events in documentation : https://thib3113.github.io/unifi-client/modules/WebSockets_events_EUnifiEvents.html
-    // else check in devtools when using the unifi web app
-    controller.on('ctrl.connect', (...args) => {
-        console.log(...args);
+    const [site] = await controller.getSites();
+    await site.initWebSockets();
+    site.ws.on('*', (eventName, ...args) => {
+        // fs.writeFileSync(path.join(__dirname, `${eventName.toLowerCase()}`), JSON.stringify(...args))
+        console.log(eventName, ...args);
     });
 };
 
