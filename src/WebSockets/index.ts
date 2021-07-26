c0af1abea4bee33fbfa2ca7f049c24a493920df7
thibaut severac <thib3113@gmail.com>
2021-07-27 00:55:16 +0200

    correct site ws + add typings

--- src/WebSockets/index.ts
+++ src/WebSockets/index.ts
@@ -1,4 +1,4 @@
 export * from './events/EControllerEvents';
-export * from './events/EUnifiEvents';
+export * from './events/events';
 export * from './Interfaces';
 export * from './UnifiWebsockets';
