e4b45a915b1da4b097389c43549808a9c0f504f1
thibaut severac <thib3113@gmail.com>
2021-07-29 19:33:11 +0200

    repair export of custom AxiosRequestConfig + add raw example + remove examples from builded + rename build to lib

---
+++ examples/raw/RawRequests.ts
@@ -0,0 +1,33 @@
+import { Controller } from '../../src';
+
+// create main function to deal with async/await
+const main = async () => {
+    const controller = new Controller({
+        username: process.env.TEST_UNIFIOS_USERNAME,
+        password: process.env.TEST_UNIFIOS_PASSWORD,
+        url: process.env.TEST_UNIFIOS_URL || 'https://unifi',
+        strictSSL: false
+    });
+
+    await controller.login();
+
+    //exemple of a raw request to the controller
+    // I want to call : https://unifi/api/users/self to get informations about mySelf
+    const mySelf = await controller.getInstance().get('/api/users/self');
+
+    // exemple of a raw request to a site
+    // I want to get sysinfo for the default site, so the request on my UDM pro is : https://unifi/proxy/network/api/s/default/stat/sysinfo
+    // I get the "default" site
+    const defaultSite = (await controller.getSites()).find((s) => s.name === 'default');
+    // I call /stat/sysinfo ... The instance will automaticaly transform for the correct url, and set the params .
+    const sysInfo = await defaultSite.getInstance().get('/stat/sysinfo');
+
+    // if I wan't to use a new V2 endpoint, like : https://unifi/proxy/network/v2/api/site/default/notifications
+    // I just need to reuse my site, call /notifications, and specify that I wan't to use apiVersion 2
+    const notifications = await defaultSite.getInstance().get('/notifications', {
+        apiVersion: 2
+    });
+};
+
+//just run the async main, and log error if needed
+main().catch((e) => console.error(e));
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,20 @@ export * from './WebSockets';
 
 export { Controller };
 export default Controller;
+
+declare module 'axios' {
+    export interface AxiosRequestConfig {
+        urlParams?: Record<string, string>;
+        site?: string;
+        apiVersion?: number;
+        /**
+         * default /proxy/network
+         */
+        unifiUrl?: string;
+        /**
+         * default false
+         */
+        authenticationRequest?: boolean;
+        retryAuth?: boolean;
+    }
+}
