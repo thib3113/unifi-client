2f5b0060317a85eb3774ee0e76b0ba6e5bf2da5a
thibaut severac <thib3113@gmail.com>
2021-08-15 16:34:41 +0200

    update jest config

--- tests/units/jest.config.js
+++ tests/units/jest.config.js
@@ -1,5 +1,11 @@
 const baseConfig = require('../jest.config.base');
+const path = require('path');
 
 module.exports = {
-    ...baseConfig
+    ...baseConfig,
+    displayName: 'units tests',
+    testMatch: baseConfig.testMatchPattern.map((pattern) => path.posix.join(__dirname, pattern)),
+    testMatchPattern: undefined
 };
+
+delete module.exports.testMatchPattern;
