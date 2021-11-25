ff7eb5a6d0c499b2167f00b0b8ebd0492f5a4e6b
thibaut severac <thib3113@gmail.com>
2021-11-26 00:01:29 +0100

    remove useless files

--- src/Devices/UXGDevice.ts
+++
@@ -1,8 +0,0 @@
-import { LANDevice } from './LANDevice';
-import { EDeviceType } from './EDeviceType';
-
-//use this class for specific functions for UXG
-export class UXGDevice extends LANDevice {
-    public static type = EDeviceType.UXG;
-    public type = EDeviceType.UXG;
-}
